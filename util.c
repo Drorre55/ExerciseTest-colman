5441867b61401231eb323b3431ad5516685676ff
Dror Regev <dror604604@gmail.com>
2026-02-01 14:34:31 +0200

    invert complete

--- util.c
+++ util.c
@@ -1,7 +1,77 @@
 #include "util.h"
+#include <math.h>
+
 
 /*
 Student Name: Dror Regev
 ID: 00000000
 */
 
+void invertRow(int A[N][N], int row) {
+	int maxIdx = N - 1;
+	int mainCrossIdx = row;
+	int secondaryCrossIdx = maxIdx - row;
+
+	// unvert row from the outside of the main and secondary crosses
+	int closestCrossIdx = mainCrossIdx <= secondaryCrossIdx ? mainCrossIdx : secondaryCrossIdx;
+	for (int i = 0; i < closestCrossIdx; i++) {
+			int leftSideSwitch = A[row][i];
+			int rightSideSwitch = A[row][maxIdx - i];
+
+			A[row][i] = rightSideSwitch;
+			A[row][maxIdx - i] = leftSideSwitch;
+	}
+}
+
+void invertCol(int A[N][N], int col) {
+	int maxIdx = N - 1;
+	int mainCrossIdx = col;
+	int secondaryCrossIdx = maxIdx - col;
+
+	int closestCrossIdx = mainCrossIdx <= secondaryCrossIdx ? mainCrossIdx : secondaryCrossIdx;
+	// unvert column from the outside of the main and secondary crosses
+	for (int i = 0; i < closestCrossIdx; i++) {
+		int upSideSwitch = A[i][col];
+		int downSideSwitch = A[maxIdx - i][col];
+
+		A[i][col] = downSideSwitch;
+		A[maxIdx - i][col] = upSideSwitch;
+	}
+}
+
+void invert(int A[N][N])
+{
+	for (int j = 0; j < N; j++) {
+		invertRow(A, j);
+		invertCol(A, j);
+	}
+}
+
+void sortWords(char*** words, int size)
+{
+}
+
+movie* searchMovieByCountry(country* c, char* moviesName)
+{
+	return;
+}
+
+int addMovieToCountry(country* c, movie* m)
+{
+	return 0;
+}
+
+movie** MoviesNotAllowedToCountry(country* c, movie** moviesPtr, int arrMoviesSize, int* arrSize)
+{
+	return;
+}
+
+int AlternatelySum(int num)
+{
+	return 0;
+}
+
+int Is11Div(int num)
+{
+	return 0;
+}
